238ef1025c09f7fd1ca466f4a72fb6d3e37ec1c0
Funan Zhou <matrix@blocksec.com>
2024-10-21 00:58:15 +0800

    fix: update README, tags and files

--- setup.py
+++ setup.py
@@ -2,8 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="chain_index",
-    version="0.1.0",
+    version="0.1.2",
     packages=find_packages(exclude=["tests*"]),
+    include_package_data=True,
+    package_data={
+        "chain_index": ["data/chains.json"]
+    },
     install_requires=[
         "pydantic>=1.8.0",
         "typing-extensions>=3.7.4",
